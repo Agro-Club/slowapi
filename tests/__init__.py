cafedb5263458275ef1651b04b9b6ef31404e87e
lishanda <sasha901239@gmail.com>
2022-06-06 22:04:57 +0300

    Modified dependencies and tests to support MongoDB

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,24 @@
 import logging
-import platform
-import unittest
-from functools import wraps
 
-import redis
+import pymongo
 from fastapi import FastAPI
 from mock import mock  # type: ignore
 from starlette.applications import Starlette
 
 from slowapi.errors import RateLimitExceeded
 from slowapi.extension import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
+
+storage_uri = 'mongodb://127.0.0.1:27017/'
+
+
+def clean_mongodb(uri):
+    myclient = pymongo.MongoClient(uri)
+    mydb = myclient["limits"]
+    mycol = mydb["counters"]
+    print(mycol)
+    mycol.drop()
 
 
 class TestSlowapi:
@@ -30,7 +37,8 @@ class TestSlowapi:
 
     def build_fastapi_app(self, config={}, **limiter_args):
         limiter_args.setdefault("key_func", get_remote_address)
-        limiter = Limiter(**limiter_args)
+        clean_mongodb(storage_uri)
+        limiter = Limiter(storage_uri=storage_uri, **limiter_args)
         app = FastAPI()
         app.state.limiter = limiter
         app.add_middleware(SlowAPIMiddleware)
